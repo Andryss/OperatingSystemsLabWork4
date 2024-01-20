26ba93f0c1d401317f93a6ceb443f46e9f85010e
Andryss <andr.krivosheev@gmail.com>
2024-01-20 12:04:12 +0300

    Refactor entrypoint

--- entrypoint.c
+++ entrypoint.c
@@ -6,28 +6,64 @@ MODULE_VERSION("0.1");
 
 char token[32];
 
-struct dentry* networkfs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flag) {
-    ino_t root;
-    struct inode *inode;
-    const char *name = child_dentry->d_name.name;
-    root = parent_inode->i_ino;
-
-    printk(KERN_INFO "Lookup in %d for %s\n", (int) root, name);
-
-    struct lookup_response response;
-    char inode_str[11];
-    snprintf(inode_str, sizeof(inode_str), "%lu", root);
-    if (networkfs_http_call(token, "lookup", &response, sizeof(response),
-                            2, "parent", inode_str, "name", name) != 0) {
-        return -1;
+int networkfs_init(void) {
+    printk(KERN_INFO "Init module start\n");
+    if (register_filesystem(&networkfs_fs_type) != 0) {
+        printk(KERN_ERR "Init module error\n");
+        return 1;
     }
+    printk(KERN_INFO "Init module successful\n");
+    return 0;
+}
 
-    printk(KERN_INFO "Found inode %lu with type %d\n", response.ino, (int) response.type);
+void networkfs_exit(void) {
+    printk(KERN_INFO "Exit module start\n");
+    if (unregister_filesystem(&networkfs_fs_type) != 0) {
+        printk(KERN_ERR "Exit module error\n");
+        return;
+    }
+    printk(KERN_INFO "Exit module successful\n");
+}
 
-    umode_t type = (response.type == DT_REG ? S_IFREG : S_IFDIR);
-    inode = networkfs_get_inode(parent_inode->i_sb, NULL, type, response.ino);
-    d_add(child_dentry, inode);
-    return NULL;
+struct dentry* networkfs_mount(struct file_system_type *fs_type, int flags, const char *t, void *data) {
+    struct dentry *ret;
+    strcpy(token, t);
+    ret = mount_nodev(fs_type, flags, data, networkfs_fill_super);
+    if (ret == NULL) {
+        printk(KERN_ERR "Can't mount file system");
+    } else {
+        printk(KERN_INFO "Mounted successfuly");
+    }
+    return ret;
+}
+
+void networkfs_kill_sb(struct super_block *sb) {
+    printk(KERN_INFO "networkfs super block is destroyed. Unmount successfully.\n");
+}
+
+int networkfs_fill_super(struct super_block *sb, void *data, int silent) {
+    struct inode *inode;
+    inode = networkfs_get_inode(sb, NULL, S_IFDIR, 100);
+    sb->s_root = d_make_root(inode);
+    if (sb->s_root == NULL) {
+        return -ENOMEM;
+    }
+    printk(KERN_INFO "Root initialized\n");
+    return 0;
+}
+
+struct inode *networkfs_get_inode(struct super_block *sb, const struct inode *dir, umode_t mode, int i_ino) {
+    struct inode *inode;
+    inode = new_inode(sb);
+    inode->i_ino = i_ino;
+    inode->i_op = &networkfs_inode_ops;
+    inode->i_fop = &networkfs_dir_ops;
+    mode |= S_IRWXU | S_IRWXG | S_IRWXO;
+    printk(KERN_INFO "Get inode of %d\n", i_ino);
+    if (inode != NULL) {
+        inode_init_owner(&init_user_ns, inode, dir, mode);
+    }
+    return inode;
 }
 
 int networkfs_iterate(struct file *filp, struct dir_context *ctx) {
@@ -73,64 +109,28 @@ int networkfs_iterate(struct file *filp, struct dir_context *ctx) {
     return response.count;
 }
 
-struct inode *networkfs_get_inode(struct super_block *sb, const struct inode *dir, umode_t mode, int i_ino) {
+struct dentry* networkfs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flag) {
+    ino_t root;
     struct inode *inode;
-    inode = new_inode(sb);
-    inode->i_ino = i_ino;
-    inode->i_op = &networkfs_inode_ops;
-    inode->i_fop = &networkfs_dir_ops;
-    mode |= S_IRWXU | S_IRWXG | S_IRWXO;
-    printk(KERN_INFO "Get inode of %d\n", i_ino);
-    if (inode != NULL) {
-        inode_init_owner(&init_user_ns, inode, dir, mode);
-    }
-    return inode;
-}
+    const char *name = child_dentry->d_name.name;
+    root = parent_inode->i_ino;
 
-int networkfs_fill_super(struct super_block *sb, void *data, int silent) {
-    struct inode *inode;
-    inode = networkfs_get_inode(sb, NULL, S_IFDIR, 100);
-    sb->s_root = d_make_root(inode);
-    if (sb->s_root == NULL) {
-        return -ENOMEM;
-    }
-    printk(KERN_INFO "Root initialized\n");
-    return 0;
-}
+    printk(KERN_INFO "Lookup in %d for %s\n", (int) root, name);
 
-struct dentry* networkfs_mount(struct file_system_type *fs_type, int flags, const char *t, void *data) {
-    struct dentry *ret;
-    strcpy(token, t);
-    ret = mount_nodev(fs_type, flags, data, networkfs_fill_super);
-    if (ret == NULL) {
-        printk(KERN_ERR "Can't mount file system");
-    } else {
-        printk(KERN_INFO "Mounted successfuly");
+    struct lookup_response response;
+    char inode_str[11];
+    snprintf(inode_str, sizeof(inode_str), "%lu", root);
+    if (networkfs_http_call(token, "lookup", &response, sizeof(response),
+                            2, "parent", inode_str, "name", name) != 0) {
+        return -1;
     }
-    return ret;
-}
 
-void networkfs_kill_sb(struct super_block *sb) {
-    printk(KERN_INFO "networkfs super block is destroyed. Unmount successfully.\n");
-}
+    printk(KERN_INFO "Found inode %lu with type %d\n", response.ino, (int) response.type);
 
-int networkfs_init(void) {
-    printk(KERN_INFO "Init module start\n");
-    if (register_filesystem(&networkfs_fs_type) != 0) {
-        printk(KERN_ERR "Init module error\n");
-        return 1;
-    }
-    printk(KERN_INFO "Init module successful\n");
-    return 0;
-}
-
-void networkfs_exit(void) {
-    printk(KERN_INFO "Exit module start\n");
-    if (unregister_filesystem(&networkfs_fs_type) != 0) {
-        printk(KERN_ERR "Exit module error\n");
-        return;
-    }
-    printk(KERN_INFO "Exit module successful\n");
+    umode_t type = (response.type == DT_REG ? S_IFREG : S_IFDIR);
+    inode = networkfs_get_inode(parent_inode->i_sb, NULL, type, response.ino);
+    d_add(child_dentry, inode);
+    return NULL;
 }
 
 module_init(networkfs_init);
